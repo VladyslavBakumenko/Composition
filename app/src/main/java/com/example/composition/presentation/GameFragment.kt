3e9c16dd1d8eaa30ffb2bd41ef6b26847d67ab5f
vladgts32@gmail.com <vladgts32@gmail.com>
2021-12-08 21:51:49 +0200

    added layouts

---
+++ app/src/main/java/com/example/composition/presentation/GameFragment.kt
@@ -0,0 +1,18 @@
+package com.example.composition.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class GameFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
